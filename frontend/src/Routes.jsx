8b35ef1bfe2f45184d407985fbc7dd62ce922255
uktony <uktony12@gmail.com>
2025-08-08 18:28:38 -0600

    fixed nested router issues

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -47,27 +47,27 @@ const AppRoutes = () => {
       {/* Protected routes with MainLayout */}
       <Route element={<ProtectedRoute><MainLayout /></ProtectedRoute>}>
         {/* Dashboard routes */}
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="settings" element={<SettingsPage />} />
         
         {/* Prediction routes */}
-        <Route path="/predictions" element={<PredictionsPage />} />
-        <Route path="/predictions/new" element={<PredictionFormPage />} />
-        <Route path="/predictions/edit/:id" element={<PredictionFormPage />} />
-        <Route path="/predictions/history" element={<PredictionHistoryPage />} />
+        <Route path="predictions" element={<PredictionsPage />} />
+        <Route path="predictions/new" element={<PredictionFormPage />} />
+        <Route path="predictions/edit/:id" element={<PredictionFormPage />} />
+        <Route path="predictions/history" element={<PredictionHistoryPage />} />
         
         {/* Analytics routes */}
-        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="analytics" element={<AnalyticsPage />} />
         
         {/* Group routes */}
-        <Route path="/groups" element={<GroupsPage />} />
-        <Route path="/groups/create" element={<CreateGroupPage />} />
-        <Route path="/groups/join" element={<JoinGroupPage />} />
-        <Route path="/groups/:groupId" element={<GroupDetailsPage />} />
-        <Route path="/groups/:groupId/predictions" element={<GroupPredictionsPage />} />
-        <Route path="/groups/:groupId/rivalries" element={<RivalryPage />} />
-        <Route path="/groups/:groupId/manage" element={<GroupManagementPage />} />
+        <Route path="groups" element={<GroupsPage />} />
+        <Route path="groups/create" element={<CreateGroupPage />} />
+        <Route path="groups/join" element={<JoinGroupPage />} />
+        <Route path="groups/:groupId" element={<GroupDetailsPage />} />
+        <Route path="groups/:groupId/predictions" element={<GroupPredictionsPage />} />
+        <Route path="groups/:groupId/rivalries" element={<RivalryPage />} />
+        <Route path="groups/:groupId/manage" element={<GroupManagementPage />} />
       </Route>
       
       {/* Home route with redirect */}
